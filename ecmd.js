22fc4194b48b39e6ffdbcd02a3c5cd66b60f186b
BrunoTC <bruno.t.couto@icloud.com>
2018-01-09 21:00:13 -0200

    update file name
    update file name

---
+++ ecmd.js
@@ -0,0 +1,56 @@
+var spawn = require('cross-spawn');
+
+function Cmd() {
+    Cmd.prototype.execute = function (command, parameters, values) {
+
+        //----- Pseudo switch -----
+        var callbacks = {};
+
+        function add(_case, fn, value) {
+            callbacks[_case] = callbacks[_case] || [];
+            callbacks[_case]['function'] = fn
+            callbacks[_case]['value'] = value
+        }
+
+        function pseudoSwitch(value) {
+            if (callbacks[value]) {
+                obj = callbacks[value]
+                console.log(obj)
+                obj['function'](obj['value']);
+            }
+        }
+
+        for (x = 0; x < values.length; x++) {
+            console.log(values)
+            add(values[x][0], function (value) {
+                child.stdin.write(value + '\n');
+            }, values[x][1]);
+        }
+        //----- Pseudo switch -----
+
+        //----- set code to be executed and execute-----
+        var child = spawn(command, parameters, {
+            stdout: 'inherit',
+            stdin: 'inherit',
+            shell: true
+        });
+        //----- set code to be executed and execute-----
+
+        console.log("Executing: " + command + " " + parameters)
+
+        //----- put values into user prompted values -----
+        child.stdout.on('data', (data) => {
+                console.log(data.toString())
+                pseudoSwitch(data.toString().trim())
+            }),
+            //----- put values into user prompted values -----
+
+            //----- End Cli -----
+            Cli.prototype.end = function () {
+                child.stdin.end();
+            }
+        //----- End Cli -----
+
+    }
+}
+module.exports = Cmd;
